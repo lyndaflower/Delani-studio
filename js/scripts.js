5ca2622e823ba7ad51b8f791a50234ada9bd171b
lyndaflower <umurerwalynda@gmail.com>
2019-07-29 15:58:22 +0200

    add change

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    $("#clickable").click(function(){
+        $("#clickable").show();
+        $("#design").toggle();
+        $("#clickable").toggle();
+    });
+    $("#design").click(function(){
+        $("#clickable").show();
+        $("#design").toggle();
+    });
+    $("#clickable2").click(function(){
+        $("#clickable2").show();
+        $("#develop").toggle();
+        $("#clickable2").toggle();
+    });
+    $("#develop").click(function(){
+        $("#clickable2").show();
+        $("#develop").toggle();
+    });
+    $("#clickable3").click(function(){
+        $("#clickable3").show();
+        $("#product").toggle();
+        $("#clickable3").toggle()
+    });
+    $("#product").click(function(){
+        $("#clickable3").show();
+        $("#product").hide();
+    });
+});
